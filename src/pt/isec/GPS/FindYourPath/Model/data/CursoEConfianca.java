aa899f81da5413e5e90e383ab8d5c20df3b018e1
blaldas <marmingues000@gmail.com>
2020-12-20 19:43:04 +0000

    Model changed. Starting to do the functions that get the solutions

---
+++ src/pt/isec/GPS/FindYourPath/Model/data/CursoEConfianca.java
@@ -0,0 +1,23 @@
+package pt.isec.GPS.FindYourPath.Model.data;
+import pt.isec.GPS.FindYourPath.Model.excelreader.Curso;
+
+public class CursoEConfianca {
+
+    private final Curso curso;
+    private final double confianca;
+
+
+    public CursoEConfianca(Curso curso, double confianca) {
+        this.curso = curso;
+        this.confianca = confianca;
+    }
+
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public double getConfianca() {
+        return confianca;
+    }
+}
--- src/pt/isec/GPS/FindYourPath/Model/data/FindYourPathData.java
+++ src/pt/isec/GPS/FindYourPath/Model/data/FindYourPathData.java
@@ -3,12 +3,15 @@ package pt.isec.GPS.FindYourPath.Model.data;
 import org.xml.sax.SAXException;
 import pt.isec.GPS.FindYourPath.Model.XMLReader.Questao;
 import pt.isec.GPS.FindYourPath.Model.XMLReader.XMLReader;
+import pt.isec.GPS.FindYourPath.Model.excelreader.Curso;
+import pt.isec.GPS.FindYourPath.Model.excelreader.ExcelFileReader;
 
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class FindYourPathData {
 
@@ -43,18 +46,135 @@ public class FindYourPathData {
     //verifica se média [9.5; 20]
     //return false:a média não pertence ao intervalo
     //return true: a média foi devidamente colocada e
-    public boolean setMedia(int n){
-        if(n < 9.5 || n > 20.0)
+    public boolean setMedia(int n) {
+        if (n < 9.5 || n > 20.0)
             return false;
         media = n;
         return true;
     }
 
+    /*
+    obtem categoria com valor máximo;           <-      escolhe categorias
+    obtem area baseado na categoria
+    obtem nome de cursos baseado na area
+    obtem cursos baseado no seu nome
+    ordena cursos baseado na proximidade da média
+    cria niveis de confianca baseado nas diferencas da média (maior ou menor)
+    escolhe 5 cursos e devolve
+     */
+
+
+    //obtem a categoria com valor máximo
+    //return: categoria com valor máximo e categorias com 90% dos pontos da cat com o valor máximo
+    private List<String> getMaxValueCategorias() {
+        String max;
+        String second;
+        String third;
+
+        //cria lista secundária para ajudar a ordenas as coisas
+        ArrayList<PontosECategoria> listaSecundaria = new ArrayList<PontosECategoria>();
+
+        //coloca "cursos" e pontos dos cursos na lista secundaria
+        listaSecundaria.add(new PontosECategoria("ciencia", catA));
+        listaSecundaria.add(new PontosECategoria("tecnologia", catB));
+        listaSecundaria.add(new PontosECategoria("economia", catC));
+        listaSecundaria.add(new PontosECategoria("negocios", catD));
+        listaSecundaria.add(new PontosECategoria("borucracia", catE));
+        listaSecundaria.add(new PontosECategoria("arte", catF));
+        listaSecundaria.add(new PontosECategoria("servicos", catG));
+        listaSecundaria.add(new PontosECategoria("cienciaTec", catH));
+        listaSecundaria.add(new PontosECategoria("tecnologiaTec", catI));
+        listaSecundaria.add(new PontosECategoria("exterior", catJ));
+        listaSecundaria.add(new PontosECategoria("negociosTec", catK));
+        listaSecundaria.add(new PontosECategoria("comunicacao", catL));
+        listaSecundaria.add(new PontosECategoria("arteTec", catM));
+        listaSecundaria.add(new PontosECategoria("servicosTec", catN));
+
+        //ordena essa lista secundaria do maior para o menor
+        listaSecundaria.sort(new CustomComparatorPontosECategoria());
+
+        //adiciona à lista a dar return o maior curso e os cursos com 90%+ dos seus ponto
+        List<String> listaSaidas = new ArrayList<String>();
+
+        listaSaidas.add(listaSecundaria.get(0).getNome());      //adiciona o primeiro curso
+
+        //enquanto houver cursos e pontos > 0.9*pontos do maior
+        for (int i = 1; i < listaSecundaria.size(); i++) {                                                  //para todos os cursos
+            if (listaSecundaria.get(i).getPontos() > 0.9 * listaSecundaria.get(0).getPontos())             //se tiver 90% pontos do maior curso
+                listaSaidas.add(listaSecundaria.get(i).getNome());                                      //adiciona
+            else                                                                                        //caso não tenha 90% dos pontos da saida com mais pontos
+                break;                                                                                  //sai do loop, uma vez que os cursos estão ordenados
+        }
+
+        return listaSaidas;
+    }
+
+
+    //recebe lista de cagorias da função getMaxValueCategorias()
+    //faz um lista com o nome dos cursos a procurar baseado na categoria
+    //devolve essa lista com TODOS os nomes de cursos baseado nessa categoria
+    private List<String> getNomeCursos(List<String> categorias) {
+        List<String> nomeCursos = new ArrayList<String>();
+
+        for (String categoria : categorias) {
+            if (categoria.equals("ciencia")) {
+
+            } else if (categoria.equals("tecnologia")) {
+
+            } else if (categoria.equals("economia")) {
+
+            } else if (categoria.equals("negocios")) {
+
+            } else if (categoria.equals("borucracia")) {
+
+            } else if (categoria.equals("arte")) {
+
+            } else if (categoria.equals("servicos")) {
+
+            } else if (categoria.equals("cienciaTec")) {
+
+            } else if (categoria.equals("tecnologiaTec")) {
+
+            } else if (categoria.equals("exterior")) {
+
+            } else if (categoria.equals("negociosTec")) {
+
+            } else if (categoria.equals("comunicacao")) {
+
+            } else if (categoria.equals("arteTec")) {
+
+            } else if (categoria.equals("servicosTec")) {
+
+            }
+
+        }
+
+        return nomeCursos;
+    }
+
+
+    private List<Curso> getListCursos(List<String> listaNomeCursos) {
+
+        List<Curso> listaCursos = new ArrayList<Curso>();       //cria uma lista que vai guardar todos os cursos
+
+        for (String nomeCurso : listaNomeCursos) {                  //para cada um dos nomes dos cursos
+
+            try {
+                //adiciona à lista de cursos todos os cursos encontrados para o nome a procurar no for each
+                listaCursos.addAll(Objects.requireNonNull(ExcelFileReader.getCursosByNome(nomeCurso)));
+
+            } catch (NullPointerException e) {      //virtualmente não acontece porque a função estsa preparada para isto. PODE é vir vazia
+            }
+        }
+
+        return listaCursos;         //devolve lista com todos os cursos
+    }
 
 
     public int getCatA() {
         return catA;
     }
+
     public void incCatA() {
         catA++;
     }
@@ -62,6 +182,7 @@ public class FindYourPathData {
     public int getCatB() {
         return catB;
     }
+
     public void incCatB() {
         catB++;
     }
@@ -69,6 +190,7 @@ public class FindYourPathData {
     public int getCatC() {
         return catC;
     }
+
     public void incCatC() {
         catC++;
     }
@@ -76,6 +198,7 @@ public class FindYourPathData {
     public int getCatD() {
         return catD;
     }
+
     public void incCatD() {
         catD++;
     }
@@ -83,6 +206,7 @@ public class FindYourPathData {
     public int getCatE() {
         return catE;
     }
+
     public void incCatE() {
         catE++;
     }
@@ -90,6 +214,7 @@ public class FindYourPathData {
     public int getCatF() {
         return catF;
     }
+
     public void incCatF() {
         catF++;
     }
@@ -97,6 +222,7 @@ public class FindYourPathData {
     public int getCatG() {
         return catG;
     }
+
     public void incCatG() {
         catG++;
     }
@@ -104,6 +230,7 @@ public class FindYourPathData {
     public int getCatH() {
         return catH;
     }
+
     public void incCatH() {
         catH++;
     }
@@ -111,6 +238,7 @@ public class FindYourPathData {
     public int getCatI() {
         return catI;
     }
+
     public void incCatI() {
         catI++;
     }
@@ -118,6 +246,7 @@ public class FindYourPathData {
     public int getCatJ() {
         return catJ;
     }
+
     public void incCatJ() {
         catJ++;
     }
@@ -125,6 +254,7 @@ public class FindYourPathData {
     public int getCatK() {
         return catK;
     }
+
     public void incCatK() {
         catK++;
     }
@@ -132,6 +262,7 @@ public class FindYourPathData {
     public int getCatL() {
         return catL;
     }
+
     public void incCatL() {
         catL++;
     }
@@ -139,6 +270,7 @@ public class FindYourPathData {
     public int getCatM() {
         return catM;
     }
+
     public void incCatM() {
         catM++;
     }
@@ -146,7 +278,46 @@ public class FindYourPathData {
     public int getCatN() {
         return catN;
     }
+
     public void incCatN() {
         catN++;
     }
+
+
+    public class PontosECategoria {
+        private int pontos;
+        private String nome;
+
+
+        public PontosECategoria(String nome, int pontos) {
+            this.pontos = pontos;
+            this.nome = nome;
+        }
+
+        public int getPontos() {
+            return pontos;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+    }
+
+    public class CustomComparatorPontosECategoria implements Comparator<PontosECategoria> {
+
+        //return 1-> primeiro é maior
+        //return 0-> sao iguais
+        //return -1 -> segundo é maior
+        @Override
+        public int compare(PontosECategoria o1, PontosECategoria o2) {
+            if (o1.getPontos() > o2.getPontos())
+                return 1;
+            else if (o1.getPontos() == o2.getPontos())
+                return 0;
+            return -1;
+        }
+    }
+
+
 }
